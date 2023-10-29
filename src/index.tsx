f0c1f41ebb1f64a9874e5f6ecce11e574360ceda
Zeu Capua <zeucapua@gmail.com>
2023-10-29 00:27:20 -0700

    implement get/post for single project

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,38 @@
 // ---------------------------------------
 
-/* 🪂Import packages (installed via npm/pnpm) */
+/* 🪂 Import packages (installed via npm/pnpm) */
 
 // Hono packages
 import { Hono } from 'hono';
 import { serve } from "@hono/node-server";
 
-// loads environment variables from `.env`, will be used later
-import * as dotenv from "dotenv";
-dotenv.config();
+// Database Driver
+import { Pool } from "pg";
+
+// Drizzle ORM packages
+import * as schema from "./schema";
+import { desc, eq } from 'drizzle-orm';
+import { drizzle } from "drizzle-orm/node-postgres";
+
+// loads environment variables from `.env`
+import "dotenv/config";
 
 // ---------------------------------------
 
-/* 🏗️Configure Hono Web Application */
+/* 🏗️ Configure Hono Web Application */
 
-// initialize web application
+// initialize web application 
 const app = new Hono();
 
-/* 🛣️Route Endpoints */
+// create pool connection to database 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+// initialize ORM client with schema types
+const database = drizzle(pool, { schema });
+
+/* 🛣️ Route Endpoints */
 
 // GET index page
 app.get("/", async (c) => {
@@ -29,12 +44,31 @@ app.get("/", async (c) => {
 
 // GET project by name
 app.get("/api/project/:name", async (c) => {
-  
+  // get route parameter (denoted with ':')
+  const name = c.req.param("name") as string;
+
+  // query database to find project with name
+  const result = await database.query.projects.findFirst({
+    where: eq(schema.projects.name, name)
+  });
+
+  // return JSON response
+  return c.json({ result });
 });
 
 // POST create project with name 
 app.post("/api/project/:name", async (c) => {
-  
+  // get route parameter (denoted with ':')
+  const name = c.req.param("name") as string;
+
+  // create a new project
+  const result = await database
+    .insert(schema.projects)
+    .values({ name })
+    .returning();
+
+  // return JSON response
+  return c.json({ result });
 });
 
 export default app;
